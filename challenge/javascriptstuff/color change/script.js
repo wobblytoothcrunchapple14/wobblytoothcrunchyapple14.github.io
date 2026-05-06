90dccf108b492011010723c09431d9f2ba03e017
wobblytoothcrunchapple14 <825259@mystma.org>
2026-05-06 14:05:59 -0500

    update

---
+++ challenge/javascriptstuff/color change/script.js
@@ -0,0 +1,25 @@
+function changeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+function changeWhite(){
+    document.body.style.backgroundColor = 'White';
+}
+
+function changeblue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+function changepink(){
+    document.body.style.backgroundColor = 'pink';
+}
+
+function changeorange(){
+    document.body.style.backgroundColor = 'orange';
+}
+
+document.getElementById('color-change').addEventListener('mouseenter', changeRed)
+document.getElementById('color-changed').addEventListener('mouseleave', changeWhite)
+document.getElementById('color-changes').addEventListener('mouseenter', changeblue)
+document.getElementById('color-chang').addEventListener('mouseenter', changeRed)
+document.getElementById('color-changged').addEventListener('mouseenter', changeRed)
