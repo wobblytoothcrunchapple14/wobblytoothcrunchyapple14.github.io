08d42e2cdbd9e3c7cd38dbc53ed98fdaf305c7f1
wobblytoothcrunchapple14 <825259@mystma.org>
2026-05-11 14:06:07 -0500

    update

--- challenge/javascriptstuff/passwordthing/script.js
+++ challenge/javascriptstuff/passwordthing/script.js
@@ -1,13 +1,19 @@
-function checkUsername() {
-    if (document.getElementById("username").value == "xX_TheDarkGamer_Xx") {
-        Access Granted
-    }
-}
-function checkPassword() {
-    if (document.getElementById("password").value == "dogwatersigmarizz6767") {
-        Access Denied
+username_text = document.getElementById("username-entry");
+password_text = document.getElementById("password-entry");
+submit_button = document.getElementById("submit-button");
+status_text = document.getElementById("status-text");
+
+
+function checkCredentials(){
+    if (username_entry.value == "admin" && password_entry.value =="admin"){
+        status_text.innerHTML = "ACCESS GRANTED";
+        status_text.style.color = "limegreen";
+} 
+
+    else{
+        status_text.innerHTML = "ACCESS DENIED";
+        status_text.style.color = "red";
     }
 }
 
-document.getElementById("username").value;
-document.getElementById("password").value;
+submit_button.addEventListener("click", checkCredentials);
